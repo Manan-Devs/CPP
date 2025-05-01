41955f2f21ad048af6f997e048c99365ed953560
Manan Bhatt <mannu1642008>
2025-05-01 14:10:44 +0530

    updated

--- 06_Star Patterns/pattern_50.cpp
+++ 06_Star Patterns/pattern_50.cpp
@@ -5,6 +5,10 @@ int main() {
    int rows;
    cout << "Enter the no. of rows :";
    cin >> rows;
-     
+      // for (int i = 1; i <= rows; i++){
+      //     if (i == 1 || i == rows){
+      //        for (int j = 1; j <=  )
+      //     }
+      // }
    return 0;
 }
