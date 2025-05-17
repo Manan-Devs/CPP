2dc4901159b065ef05e295fe85230397f53cf70b
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-17 10:18:39 +0530

    Update functionsHomework2.cpp

--- 07_Functions/functionsHomework2.cpp
+++ 07_Functions/functionsHomework2.cpp
@@ -52,6 +52,14 @@ using namespace std;
      //cout << "Enter your temperature in celsius : " << endl;
      //cin >> celsius;
      //cout << "Your given temperature in fahrenheit : " << celsiusToFahrenheit(celsius) << endl;
-    
+      int N;
+      cout << "Enter your decimal number : " << endl;
+      cin >> N;
+      int i;
+      cout << "Set your ith bit : " << endl;
+      cin >> i;
+      cout << "Your binary number is : " << setIthBit(N , i) << endl;
+      cout << setIthBit(N , i) << endl;
+      return 0;
      
 }
