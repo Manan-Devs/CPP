bd2cbf7a2c5494594d98d1123f0d398a3c73f5ff
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-18 15:56:51 +0530

    Function problem solution

--- 08_Function problems/Problem1.cpp
+++ 08_Function problems/Problem1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+
+// function to find out the sum of digits of until number becomes zero
+ int sumOfDigits(int num) {
+    int sum = 0;
+     for (int i = num; i > 0; i = i/10){
+       sum+= i % 10;
+     }
+     return sum;
+ }
+
+
 int main() {
-    
+    int num;
+    cout << "Enter your number" << endl;
+    cin >> num;
+    cout << "Sum of digits of your number: " << sumOfDigits(num) << endl; 
     return 0;
 }
