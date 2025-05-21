c858cdb15c5a72bc743076c6bb8b2a59f1a2a595
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-21 10:58:07 +0530

    Update Problem8.cpp

--- 08_Function problems/Problem8.cpp
+++ 08_Function problems/Problem8.cpp
@@ -2,13 +2,4 @@
 
 // function to print diamond pattern of stars 
   diamondPattern(int size) {
-   
  
-
-
-
-    cout << "Enter your size: ";
-    cin >> size;
-    diamondPattern(size);
-    return 0;
-}
