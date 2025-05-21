b59f3a6e248e3ceccb7db9d718560ce75674c467
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-21 10:54:21 +0530

    Add files via upload

---
+++ 08_Function problems/Problem9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+ void multiplicationMatrix(int size) {
+     for (int i = 1; i <= size; i++) {
+         for (int j = 1; j <= size; j++) {
+             cout << " " << i * j;
+         }
+       cout << endl;
+     }
+ }
+
+
+
+
+int main() {
+     int size;
+     cout << "Enter your size : ";
+     cin >> size;
+     multiplicationMatrix(size);
+     return 0;
+}
