269cd29dab0072ed8e586a82e56c3602494982cf
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-21 10:55:24 +0530

    Add files via upload

---
+++ 08_Function problems/Problem8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// function to print diamond pattern of stars 
+ void diamondPattern(int size) {
+       
+ }
+
+
+
+
+
+int main() {
+    int size;
+    cout << "Enter your size: ";
+    cin >> size;
+    diamondPattern(size);
+    return 0;
+}
