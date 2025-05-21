55b5eed07e60dd11daa1cc19506c84523cda7501
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-21 17:07:34 +0530

    Add files via upload

---
+++ 08_Function problems/Problem12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int  sumSquaresUptoNeven(int N) {
+     int sum = 0;
+     for (int i = 2; i <= N; i = i + 2) {
+         sum = sum + (i*i);
+     }
+     return sum;
+}
+
+
+int main() {
+    int N;
+    cout << "Enter your N: ";
+    cin >> N;
+    cout << "Sum of squares of All Even Numbers Up to" << " " << N << " " << "are" <<  " " << sumSquaresUptoNeven(N);
+    return 0;
+}
