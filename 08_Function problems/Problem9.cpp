01781bc36e5a5f79d1be458bb9dd4004a8d06fe2
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-28 21:46:29 +0530

    Update Problem9.cpp

--- 08_Function problems/Problem9.cpp
+++ 08_Function problems/Problem9.cpp
@@ -5,7 +5,7 @@ using namespace std;
      for (int i = 1; i <= size; i++) {
          for (int j = 1; j <= size; j++) {
              cout << " " << i * j;
-         }
+       
        cout << endl;
      
  }
