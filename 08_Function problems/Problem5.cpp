652772472a74320ed4bc2020e8bf7877a62b4898
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-05-28 21:50:58 +0530

    Update Problem5.cpp

--- 08_Function problems/Problem5.cpp
+++ 08_Function problems/Problem5.cpp
@@ -10,7 +10,7 @@ int GCD(int num1 , int num2 , int num3)   {
       else if (num1 % num2 == 0 && num2 % num2 == 0 && num3 % num2 == 0) {
            GCD = num2;
       }
-       else if (num1 % num3 == 0 && num2 % num3 == 0 && num3 % num3 == 0) {
+       else if (num % num3 == 0 && num2 % num3 == 0 && num3 % num3 == 0) {
            GCD = num3;
        }
      return GCD;
