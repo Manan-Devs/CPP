a1c0fab7e0c17809def86ddb55f449e81b1ed9e9
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-06-22 15:19:50 +0530

    Update Problem15.cpp

--- 08_Function problems/Problem15.cpp
+++ 08_Function problems/Problem15.cpp
@@ -18,6 +18,31 @@ using namespace std;
      return count;
  }
 
+ // Optimized Code 
+//  bool isPerfect(int num) {
+//     int sum = 1;  // 1 is always a divisor (except for num = 1)
+//     if (num == 1) return false;
+
+//     for (int i = 2; i <= sqrt(num); i++) {
+//         if (num % i == 0) {
+//             sum += i;
+//             int paired = num / i;
+//             if (paired != i) sum += paired; // avoid adding square root twice
+//         }
+//     }
+
+//     return sum == num;
+// }
+
+// int perfectCheck(int limit) {
+//     int count = 0;
+//     for (int i = 2; i <= limit; i++) {
+//         if (isPerfect(i)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
 
 int main() {
