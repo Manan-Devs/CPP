c08f191bca0b28b39591525fe1d2cdf29caa1ba9
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-06-24 17:48:45 +0530

    New function problem

--- 08_Function problems/Problem17.cpp
+++ 08_Function problems/Problem17.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
+#include <cmath>
+#include <typeinfo>
 using namespace std;
 
  // function to check the perfect squares in a range 
  void checkPerfectSquares(int start, int end){
-    for (int i = start; i <= end; i++){
-       if (i * i < end){
-         cout << " " << i * i;
-       }
-  }
+       for(int index = start; index < end; index++){
+           int squareRoot = sqrt(index*index);
+           if (typeid(squareRoot) == typeid(int)) {
+               cout << index * index << " ";
+           }
+         }
  }
-  
-
-
-
-int main() {
+  int main() {
     int start;
     int end;
     cout << "Enter your start value : " ;
     cin >> start;
-    cout << "Entery your end value : " ;
+    cout << "Enter your end value : " ;
     cin >> end;
     cout << "[";
     checkPerfectSquares(start , end);
