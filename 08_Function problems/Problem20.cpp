528a8c97a65c57cffcce91cbd3531ed732c2e9d2
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-06-27 22:27:11 +0530

    Update Problem20.cpp with

--- 08_Function problems/Problem20.cpp
+++ 08_Function problems/Problem20.cpp
@@ -1,33 +1,81 @@
 #include <iostream>
-#include <vector> // uses vector 
+#include <limits.h>
+#include <cmath> 
+#include <vector>  
 using namespace std;
  
-// function to find the largest prime factor of a given number
-   void largestPrime(int num) {
-     vector<int> largestPrime;
-     // find all prime factors of the number
-     for(int i = 2; i < num; i++){
+// function to find the largest prime factor of any number 
+ vector <int> primeFactors;
+
+  void isPrime(int n) {
+     for(int i = 2; i < n; i++){
          bool checkPrime = true;
-         for (int j = i - 1; j > 1; j--){
+         for (int j = 2; j <= sqrt(i); j++){
              if (i % j == 0){
-                 checkPrime = false;
-                 break;
+                checkPrime = false;
+                break;
              }
          }
-         // check number is prime and if it divides the given number
-          if (checkPrime == true && num % i == 0) {
-             largestPrime.push_back(i);
-            }
+          if (checkPrime && n % i == 0){
+              primeFactors.push_back(i);
+          }
     }
-     // Print largest prime factor
-    cout << largestPrime.back() << " ";
- }
-    int main() {
-    int num;
+  }
+
+  int main() {
+    int n;
     cout << "Enter your number: ";
-    cin >> num;
-    cout << "Largest prime factor of your given number is: ";
-    cout << endl;
-    largestPrime(num);
-    return 0;
+    cin >> n;
+    cout << "The largest prime factor of " << n << " is: ";
+    isPrime(n);
+    // find the maximum prime factor from the vector array 
+    int Maxi = INT_MIN;
+    for(int i = 0; i < primeFactors.size(); i++){
+         if (primeFactors[i] > Maxi) {
+             Maxi = primeFactors[i];
+     }
 }
+    cout << Maxi << endl;
+}
+
+
+
+// ✅ Optimized & Clean Version:
+// cpp
+// Copy
+// Edit
+// #include <iostream>
+// using namespace std;
+
+// long long largestPrimeFactor(long long n) {
+//     long long largest = -1;
+
+//     // Divide by 2 until odd
+//     while (n % 2 == 0) {
+//         largest = 2;
+//         n /= 2;
+//     }
+
+//     // Try all odd factors up to sqrt(n)
+//     for (long long i = 3; i * i <= n; i += 2) {
+//         while (n % i == 0) {
+//             largest = i;
+//             n /= i;
+//         }
+//     }
+
+//     // If anything is left of n and greater than 2, it's prime
+//     if (n > 2) {
+//         largest = n;
+//     }
+
+//     return largest;
+// }
+
+// int main() {
+//     long long n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     cout << "The largest prime factor of " << n << " is: " << largestPrimeFactor(n) << endl;
+//     return 0;
+// }
