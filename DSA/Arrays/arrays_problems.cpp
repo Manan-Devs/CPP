442d7d46ef32877e1ee30b89121f2559106ce5d9
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-06-29 18:06:32 +0530

    Arrays problems

---
+++ DSA/Arrays/arrays_problems.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+//   // function to insert element at a given position
+//   int count = 0;
+//   void insertion(int n , int pos , int x){
+//      //creation of array
+//       int arr[n];
+//       //taking elements of the array
+//       for(int i = 0; i < n; i++){
+//          cin >> arr[i];
+//          count++;
+//       }
+//       if (count != n){
+//          cout << "Please enter no. of elements match with the size of the array." << endl;
+//        }
+//          //insertion loop
+//           for(int i = n - 1; i > pos; i--){
+//               arr[i] = arr[i - 1]; // shifting elements to the right
+//               arr[pos] = x; // inserting the element at the specified position
+//         }
+//         //  printing loop
+//             for(int i = 0; i < n; i++){
+//               cout << arr[i] << " "; // printing the array after insertion
+//             }
+//     }
+
+//     int main() {
+//     int n;
+//     int x;
+//     int pos;
+//     cout << "enter your size: ";
+//     cin>> n;
+//     cout << "enter your element to add in array: ";
+//     cin >> x;
+//     cout << "enter your position for insertion: ";
+//     cin >> pos;
+//      //checking all edgeCases 
+//        if(n <= 0 || n >= 1000000 || n == 1 || pos <= 0 || pos >= n || pos == n-1 || x < 0 || x >= 1000000){
+//         cout << "Invalid input. Please ensure that all inputs are within the valid range." << endl;
+//            return 0; // Exit the program here 
+//         }
+//       cout << "Enter elements of your array: ";
+//       insertion(n , pos , x);
+//       return 0;
+// }
+          
+   
+ 
+ int main() {
+     int n;
+     int delIndex;
+     cout << "Enter the size of the array: ";
+     cin >> n;
+     int arr[n];
+     cout << "Enter the delIndex: ";
+     cin >> delIndex;
+     cout << "Enter the elements of the array: ";
+     for(int i = 0; i < n; i++){
+       cin >> arr[i];
+     }
+       for(int i = delIndex; i < n - 1; i++){
+         arr[i] = arr[i + 1]; // shifting elements to the left
+       } 
+      
+     for(int i = 0; i < n - 1; i++){
+         cout << arr[i] << " ";
+     }
+   return 0;
+ }
