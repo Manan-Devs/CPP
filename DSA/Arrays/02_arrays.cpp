b31411ebaf4edba85ae6a0b8ab682fe4dd80e6fb
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-07-10 18:26:41 +0530

    Update 02_arrays.cpp

--- DSA/Arrays/02_arrays.cpp
+++ DSA/Arrays/02_arrays.cpp
@@ -186,8 +186,8 @@ int main() {
 
 
     // print Triplets match with our sum 
-  vector <int> arr{10 , 20 , 30 , 40};
-  int sum = 80;
+  // vector <int> arr{10 , 20 , 30 , 40};
+  // int sum = 80;
        
          // outer loop will traverse for each element 
   for(int i = 0; i < arr.size(); i++){
