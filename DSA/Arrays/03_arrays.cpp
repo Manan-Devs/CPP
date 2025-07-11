0dc72b762ffd807733c9980e4079e1b5b0fc3cf9
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-07-11 22:01:29 +0530

    New file with new code

---
+++ DSA/Arrays/03_arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+     // declare 2d array
+     int arr[3][3];
+
+     //intialisation
+    int brr[3][3] = {
+      {1 , 2 , 3},
+      {4 , 5 , 6},
+      {2 , 4 , 8}};
+    
+  // row - wise accessing 
+   for(int i = 0; i < 3; i++){
+      for(int j = 0; j < 3; j++){
+         cout << brr[i][j] << " ";
+      }
+     cout << endl;
+   }
+   cout << "This is the row - wise printing";
+   
+   
+   
+   
+   
+      
+   // cout << brr[2][2] << endl;
+}
