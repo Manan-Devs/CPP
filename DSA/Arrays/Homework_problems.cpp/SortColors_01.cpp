ac5adfdd163ac643f0b2f370f3e217b63f56af63
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-07-19 18:01:07 +0530

    Add files via upload

---
+++ DSA/Arrays/Homework_problems.cpp/SortColors_01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+  vector <int> arr{2, 0, 1, 2, 1, 0, 1, 2, 0, 1};
+  int mid = arr.size() - 1;
+
+  for(int i = 0; i < arr.size(); i++){
+      
+ }
+  for(int i = 0; i < arr.size(); i++){
+     cout << arr[i] << " ";
+  }
+}
+
+
+
---
+++ DSA/Arrays/Homework_problems.cpp/Sprial print_02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function to print Sprial matrix 
+
+
+
+
+
+
+int main() {
+    
+}
