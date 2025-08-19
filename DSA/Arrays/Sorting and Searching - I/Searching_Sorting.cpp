c75b0620ecee0e1b322e12ca45c38fa256750daa
Manan Bhatt <95690914+Manan-Devs@users.noreply.github.com>
2025-08-19 21:06:31 +0530

    Update Searching_Sorting.cpp

--- DSA/Arrays/Sorting and Searching - I/Searching_Sorting.cpp
+++ DSA/Arrays/Sorting and Searching - I/Searching_Sorting.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // Binary Search Code 
 int binarySearch(int arr[] , int size , int target){
      int start = 0
-     int end = size - 1;
+     int end = size - 1
 
      int mid = (start + end) / 2;
     
@@ -66,3 +66,4 @@ int main() {
 
 
 }
+
